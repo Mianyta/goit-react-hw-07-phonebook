dbf07cb67c4c7ed46b588b8ab2a44770ca54c073
Mianyta <suta180987@gmail.com>
2023-11-26 17:28:11 +0200

    new fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import React, { useEffect } from 'react';
+import { TitlePhonebook } from './TitlePhonebook/TitlePhonebook';
+import { TitleContacts } from './TitleContacts/TitleContacts';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Container } from './Container/Container.styled';
+import { Section } from './Section/Section.styled';
+import { Loader } from './Loader/loader';
+import { selectContactError, selectContactIsLoading } from 'redux/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import ErrMessage from './ErrMessage/ErrMessage';
+import { fetchContacts } from '../redux/operations';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectContactIsLoading);
+  const error = useSelector(selectContactError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Section>
+        <TitlePhonebook title="Phonebook" />
+        <ContactForm />
+      </Section>
+      <Section>
+        <TitleContacts title="Contacts" />
+        <Filter />
+        <ContactList />
+        {isLoading && <Loader />}
+        {error && <ErrMessage />}
+      </Section>
+    </Container>
   );
 };
